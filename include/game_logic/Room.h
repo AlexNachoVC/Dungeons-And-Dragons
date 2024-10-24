f9822daa8e62088ffeee8ea9dc3b3a5d07556666
Alejandro <alexnachovc12@gmail.com>
2024-10-23 23:26:27 -0600

    DnD, Room class header declaration

--- include/game_logic/Room.h
+++ include/game_logic/Room.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <iostream>
+#include "Monster.h"
+#include "Catalog.h"
+using namespace std;
+
+class Room {
+private:
+    Monster roomMonster;
+
+public:
+    Room();
+    ~Room();
+    bool assignRandomMonster(MonsterCatalog);
+};

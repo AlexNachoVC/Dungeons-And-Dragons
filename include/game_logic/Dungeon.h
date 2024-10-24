5abe8012d3d0ed96710142a524ecfd65c140dbdd
Alejandro <alexnachovc12@gmail.com>
2024-10-23 23:29:39 -0600

    DnD, Dungeon class header declaration

--- include/game_logic/Dungeon.h
+++ include/game_logic/Dungeon.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <iostream>
+#include "Room.h"
+#include "lib/templates/DoublyLinkedList.hpp"
+
+using namespace std;
+
+class Dungeon {
+private:
+    DoublyLinkedList<Room> Rooms;
+
+public:
+    Dungeon();
+    ~Dungeon();
+    void traverseDungeon();
+};

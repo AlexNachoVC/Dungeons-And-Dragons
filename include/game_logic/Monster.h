e695ca69c9f840d4cbda941b42319d6a11fc183d
Alejandro <alexnachovc12@gmail.com>
2024-10-23 23:23:52 -0600

    DnD, Monster class header declaration

--- include/game_logic/Monster.h
+++ include/game_logic/Monster.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class Monster {
+private:
+    string name;
+    int challengeRating;
+    string type;
+    string size;
+    int armorClass;
+    int hitPoints;
+    string alignment;
+
+public:
+    Monster();
+    ~Monster();
+
+    string getName();
+    int getChallengeRating();
+    string getType();
+    string getsSize();
+    int getArmorClass();
+    int getHitPoints();
+    string getAlignment();
+
+    void setName(string name);
+    void setChallengeRating(int challengeRating);
+    void setType(string type);
+    void setsSize(string size);
+    void setArmorClass(int armorClass);
+    void setHitPoints(int hitPoints);
+    void setAlignment(string alignment);
+
+    void displayStats();
+};

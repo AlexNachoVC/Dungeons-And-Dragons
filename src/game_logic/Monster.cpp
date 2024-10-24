7f9257b4f8ba4bafb31d209b8c0ecd084fe07bf6
Alejandro <alexnachovc12@gmail.com>
2024-10-24 11:32:07 -0600

    DnD, Monster class implementation, missing tests

--- src/game_logic/Monster.cpp
+++ src/game_logic/Monster.cpp
@@ -1,1 +1,91 @@
 #include "../../include/game_logic/Monster.h"
+#include "Monster.h"
+
+Monster::Monster() : name(""), challengeRating(0), type(""), size(""), armorClass(0), hitPoints(0), alignment("") {}
+
+Monster::Monster(string name, int challengeRating, string type, string size, int armorClass, int hitPoints, string alignment) {
+    this->name = name;
+    this->challengeRating = challengeRating;
+    this->type = type;
+    this->size = size; 
+    this->armorClass = armorClass;
+    this->hitPoints = hitPoints;
+    this->alignment = alignment;
+}
+
+Monster::~Monster() {
+
+}
+
+string Monster::getName()
+{
+    return name;
+}
+
+int Monster::getChallengeRating()
+{
+    return challengeRating;
+}
+
+string Monster::getType()
+{
+    return type;
+}
+
+string Monster::getsSize()
+{
+    return size;
+}
+
+int Monster::getArmorClass()
+{
+    return armorClass;
+}
+
+int Monster::getHitPoints()
+{
+    return hitPoints;
+}
+
+string Monster::getAlignment()
+{
+    return alignment;
+}
+
+void Monster::setName(string name) {
+    this->name = name;
+}
+
+void Monster::setChallengeRating(int challengeRating) {
+    this->challengeRating = challengeRating;
+}
+
+void Monster::setType(string type) {
+    this->type = type;
+}
+
+void Monster::setsSize(string size) {
+    this->size = size;
+}
+
+void Monster::setArmorClass(int armorClass) {
+    this->armorClass = armorClass;
+}
+
+void Monster::setHitPoints(int hitPoints) {
+    this->hitPoints = hitPoints;
+}
+
+void Monster::setAlignment(string alignment) {
+    this->alignment = alignment;
+}
+
+void Monster::displayStats() {
+    cout << "Name: " << this->name << endl;
+    cout << "Challenge Rating: " << this->challengeRating << endl;
+    cout << "Type: " << this->type << endl;
+    cout << "Size: " << this->size << endl;
+    cout << "Armor Class: " << this->armorClass << endl;
+    cout << "Hit Points: " << this->hitPoints << endl;
+    cout << "Alignment: " << this->alignment << endl;
+}

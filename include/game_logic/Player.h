6782a022656353e938ccc6ce3b829776271b272f
Alejandro <alexnachovc12@gmail.com>
2024-10-23 23:18:09 -0600

    DnD, Player class header implementation

--- include/game_logic/Player.h
+++ include/game_logic/Player.h
@@ -1,1 +1,33 @@
 #pragma once
+
+#include <iostream>
+#include "lib/templates/SinglyLinkedlist.hpp"
+#include "Monster.h"
+
+using namespace std;
+
+class Player {
+private:
+    int hitPoints;
+    string race;
+    string name;
+    int magicPoints;
+    LinkedList<Monster> defeatedMonsters;
+
+public:
+    Player();
+    ~Player();
+
+    int getHitPoints();
+    string getRace();
+    string getName();
+    int getMagicPoints();
+
+    void setHitPoints(int hitPoints);
+    void setRace(string race);
+    void setName(string name);
+    void setMagicPoints(int magicPoints);
+
+    bool addDefeatedMonster(Monster);
+    void displayDefeadedMonsters();
+};
